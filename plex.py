a643d51e8728dd20ae37d2946dd64bfbe27748b0
scottcase <scott.case.1@gmail.com>
2019-03-06 20:03:01 -0500

    sad

--- plex.py
+++ plex.py
@@ -113,7 +113,8 @@ def scan(config, lock, path, scan_for, section, scan_type, resleep_paths):
         logger.info(final_cmd)
         
         # final_cmd = 'sudo -u %s bash -c %s' % (config['PLEX_USER'], cmd_quote(cmd))
-        mydata = '-i Plex bash -c "%s" ' % (final_cmd)
+        # mydata = '-i Plex bash -c "%s" ' % (final_cmd)
+        mydata = "-i Plex bash -c \"/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --section 44 --directory '/mnt/user/TVShows/FamilyTVShows/Ghost.Whisperer/Season.02'\""
         logger.info(mydata)
         dkrVer = client.exec_run(mydata, user=abc)
         logger.info("right below shound show something about docker version")
